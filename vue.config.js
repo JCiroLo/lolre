d1eb261a432b8ca1c448ddcbc6b61d261531d183
JCiroLo <juanciro35@gmail.com>
2023-03-30 15:07:38 -0500

    Express server was removed

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
-module.exports ={
-    css: {
-        loaderOptions:{
-            scss: {
-                prependData: `@import "./src/assets/scss/_Config.scss";`,
-            }
-        }
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/lolre/' : '/',
+
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `@import "./src/assets/scss/_Config.scss";`
+      }
     }
+  }
 }
